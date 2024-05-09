1d101a611205b59361a4919e6e24934add8b814b
BenManeno <lubangwemaneno20@gmail.com>
2024-05-09 14:52:56 -0400

    final changes(2)
    making the font big, change the text hint  color to black

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "org.pattersonclippers.englishlearning"
         minSdk = 19
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
